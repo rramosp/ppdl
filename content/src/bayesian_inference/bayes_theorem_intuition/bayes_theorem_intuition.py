fdf28ea41ac673e3ffad1f343083c75b23490801
Felipe-RA <felipeangel50@gmail.com>
2022-01-24 18:16:07 -0500

    added animations until min 3

--- content/src/bayesian_inference/bayes_theorem_intuition/bayes_theorem_intuition.py
+++ content/src/bayesian_inference/bayes_theorem_intuition/bayes_theorem_intuition.py
@@ -1,15 +1,101 @@
 import logging
 import os, sys
 
+from numpy import vdot
+
 sys.path.insert(0, ".")
 from manim import *
 from lib.scenes import *
 from lib.objects import *
 from lib.utils import *
 
+ES_text_dict = {}
+
+def create_bayes_theorem_header(scene):
+    ES_text_dict["bayes_theorem_header"] = "Definición del Teorema de Bayes"
+    text_bayes = Text(ES_text_dict["bayes_theorem_header"], color=BLUE_E ).to_edge(UP)
+    scene.play(Write(text_bayes))
+    return text_bayes
+
+def create_bayes_theorem_definition(scene,header):
+    
+    text_latex_bayes_def_left = Tex(r'$$P(A|B)$$').scale(1)
+    text_latex_bayes_def_right = Tex(r'$$ = \frac{P(B|A)P(A)}{P(B)}$$').scale(1)
+
+    pairs = [("left",text_latex_bayes_def_left) , ("right", text_latex_bayes_def_right)]
+    group_latex_bayes_def = VDict(pairs,show_keys=False)
+
+    text_latex_bayes_def_left.next_to(text_latex_bayes_def_right,direction=LEFT)
+    group_latex_bayes_def.next_to(header,direction=DOWN)
+
+    scene.play(Write(group_latex_bayes_def),lag_ratio=0.5)
+
+    return group_latex_bayes_def
+
+def create_bayesian_inference_header(scene, split_line):
+    ES_text_dict["bayesian_inference_1"] = "Inferencia Bayesiana 1"
+    ES_text_dict["bayesian_inference_2"] = "Inferencia Bayesiana 2"
+
+    text_keypoint_one = Text(ES_text_dict["bayesian_inference_1"],color=BLUE_E,font_size=24).to_edge(LEFT)
+    text_keypoint_two = Text(ES_text_dict["bayesian_inference_2"],color=BLUE_E,font_size=24).to_edge(RIGHT)
+
+    pairs = [("left_keypoint", text_keypoint_one) , ("right_keypoint", text_keypoint_two)]
+    group_keypoints_inference = VDict(pairs,show_keys=False)
+
+    text_keypoint_one.align_to(split_line,UP)
+    text_keypoint_two.align_to(split_line,UP)
+
+    scene.play(Write(group_keypoints_inference))
+    return group_keypoints_inference
+
+def create_inference_bulletpoint_one(scene, inference_bulletpoints_header):
+    ES_text_dict["bayesian_inference_belief"] = "Conocimiento"
+    ES_text_dict["bayesian_inference_uncertainty"] = "Incertidumbre"
+
+    text_belief = Text(ES_text_dict["bayesian_inference_belief"],color=BLACK,font_size=20).to_edge(LEFT,buff=1.5)
+    text_uncertainty = Text(ES_text_dict["bayesian_inference_uncertainty"],color=BLACK,font_size=20)
+
+    text_belief.align_to(inference_bulletpoints_header["left_keypoint"],DOWN)
+    text_belief.shift(DOWN*0.8)
+    
+    text_uncertainty.next_to(text_belief,RIGHT*4)
+
+    double_arrow_belief_uncertainty = DoubleArrow(start=text_belief.get_right(),end=text_uncertainty.get_left(), color= BLUE_E)
+
+    pairs = [("belief", text_belief) ,
+             ("uncertainty", text_uncertainty),
+             ("double_arrow" , double_arrow_belief_uncertainty) 
+            ]
+
+    header_keypoint_one = VDict(pairs, show_keys=False)
+
+    scene.play(Write(text_belief))
+    scene.play(Write(double_arrow_belief_uncertainty))
+    scene.play(Write(text_uncertainty))
+
+    return header_keypoint_one
+
+def create_inference_bulletpoint_one_example(scene, inference_bulletpoint_one):
+
+    text_latex_rain = Tex("$$P(rain) = $$").scale(1)
+    text_latex_rain.to_corner(DL,buff=2)
 
 
+    decimal = DecimalNumber(0.5, color=BLACK)
+    decimal.next_to(text_latex_rain, direction=RIGHT)
+    
 
+    scene.play(Write(text_latex_rain))
+    scene.play(FadeIn(decimal))
+    scene.wait(10)
+    scene.play(Indicate(inference_bulletpoint_one["uncertainty"],color=RED_E,run_time=4))
+    scene.wait(30)
+    scene.play(FadeOut(decimal))
+    decimal.set_value(1.0)
+    scene.play(FadeIn(decimal))
+    scene.wait(2)
+    scene.play(Indicate(inference_bulletpoint_one["belief"],color=RED_E,run_time=4))
+    return text_latex_rain,decimal
 
 
 
@@ -21,4 +107,37 @@ class Main(Scene):
         sfile = find_soundfile("2022-01-12 15-40-29-bayes-theorem")
 
         self.add_sound(sfile)
+        bayes_theorem_intro = create_bayes_theorem_header(self)
+        self.wait_until(11)
+
+        bayes_latex_theorem = create_bayes_theorem_definition(self, bayes_theorem_intro)
+        self.wait_until(35)
+
+        self.play(Indicate(bayes_latex_theorem["left"]),color=RED_E,run_time=4)
+        self.wait_until(45)
         
+        dot_bottom = Dot(color=BLUE_E)
+        dot_bottom.to_edge(DOWN)
+
+        split_line = Line(bayes_latex_theorem.get_bottom(), dot_bottom.get_center(),color=BLUE_E)
+        self.play(Create(split_line))
+        self.play(Create(dot_bottom))
+
+        self.wait_until("1min 2sec")
+        self.play(bayes_latex_theorem.animate.scale(0.6))
+        inference_bulletpoints_header = create_bayesian_inference_header(self,split_line)
+
+        self.wait_until("1min 15sec")
+        self.play(Indicate(bayes_latex_theorem),color=RED_E,run_time=3)
+
+        self.wait_until("1min 36sec")
+        inference_bulletpoint_one = create_inference_bulletpoint_one(self,inference_bulletpoints_header)
+
+        self.wait_until("1min 55sec")
+
+        inference_keypoint_one_example =  create_inference_bulletpoint_one_example(self,inference_bulletpoint_one)
+        self.wait_until("2min 45sec")
+
+
+        self.wait_until("17min 38sec")
+
