b30403920a57e51509420c6ebd529d9d05ee20fe
Felipe-RA <felipeangel50@gmail.com>
2022-01-25 11:36:56 -0500

    advances on video

--- content/src/bayesian_inference/01_bayes_theorem_intuition/bayes_theorem_intuition.py
+++ content/src/bayesian_inference/01_bayes_theorem_intuition/bayes_theorem_intuition.py
@@ -97,24 +97,62 @@ def create_inference_bulletpoint_one_example(scene, inference_bulletpoint_one):
     scene.play(Indicate(inference_bulletpoint_one["belief"],color=RED_E,run_time=4))
     return text_latex_rain,decimal
 
+def create_inference_bulletpoint_two(scene, inference_bulletpoints_header):
+
+    text_belief = Text(ES_text_dict["bayesian_inference_belief"],color=BLACK,font_size=22).to_edge(RIGHT,buff=2.7)
+
+    text_belief.align_to(inference_bulletpoints_header["right_keypoint"],DOWN)
+    text_belief.shift(DOWN*0.8)   
+
+    pairs = [("belief", text_belief)
+            ]
+
+    header_keypoint_two = VDict(pairs, show_keys=False)
+
+    scene.play(Write(text_belief))
+    scene.play(Circumscribe(text_belief,color=BLUE_B,time_width=3,fade_out=True))
+    scene.play(Circumscribe(text_belief,color=BLUE_B,time_width=3,fade_out=True))
+    scene.play(Circumscribe(text_belief,color=BLUE_B,time_width=3))
+
+
+
+    return header_keypoint_two
+
+
+def create_inference_bulletpoint_two_example(scene, inference_bulletpoints_header):
+    ES_text_dict["list_bulletpoint_two"] = "- Observaciones\n- Eventos\n- Información"
+
+    txt = Text(ES_text_dict["list_bulletpoint_two"],color=BLACK,font_size=16).to_edge(RIGHT, buff= 5)
+
+    txt.align_to(inference_bulletpoints_header["right_keypoint"],DOWN)
+    txt.shift(DOWN*2)
+
+    scene.play(Write(txt,run_time=8.0))
+
+
+
+
 
 
 class Main(Scene):
     def construct(self):
+
         video_name = r"Bayes theorem intuition"
         play_intro_scene(self,video_name)
-        timer = SceneTimer(self,debug_wait=False).reset()
-        sfile = find_soundfile("2022-01-12 15-40-29-bayes-theorem")
+        timer = SceneTimer(self,debug_wait=True).reset()
 
+        sfile = find_soundfile("2022_01_12_15_40_29_bayes_theorem")
+        timer.wait_until(3)
         self.add_sound(sfile)
+        
         bayes_theorem_intro = create_bayes_theorem_header(self)
-        self.wait_until(11)
+        timer.wait_until(11)
 
         bayes_latex_theorem = create_bayes_theorem_definition(self, bayes_theorem_intro)
-        self.wait_until(35)
+        timer.wait_until(35)
 
         self.play(Indicate(bayes_latex_theorem["left"]),color=RED_E,run_time=4)
-        self.wait_until(45)
+        timer.wait_until(45)
         
         dot_bottom = Dot(color=BLUE_E)
         dot_bottom.to_edge(DOWN)
@@ -123,21 +161,31 @@ class Main(Scene):
         self.play(Create(split_line))
         self.play(Create(dot_bottom))
 
-        self.wait_until("1min 2sec")
+        timer.wait_until("1min 2sec")
         self.play(bayes_latex_theorem.animate.scale(0.6))
         inference_bulletpoints_header = create_bayesian_inference_header(self,split_line)
 
-        self.wait_until("1min 15sec")
+        timer.wait_until("1min 15sec")
         self.play(Indicate(bayes_latex_theorem),color=RED_E,run_time=3)
 
-        self.wait_until("1min 36sec")
-        inference_bulletpoint_one = create_inference_bulletpoint_one(self,inference_bulletpoints_header)
+        timer.wait_until("1min 33sec")
+        self.play(Indicate(inference_bulletpoints_header["left_keypoint"]), color=RED_E,run_time=2)
 
-        self.wait_until("1min 55sec")
+        timer.wait_until("1min 36sec")
+        inference_bulletpoint_one = create_inference_bulletpoint_one(self,inference_bulletpoints_header)      
 
+        timer.wait_until("1min 55sec")
         inference_keypoint_one_example =  create_inference_bulletpoint_one_example(self,inference_bulletpoint_one)
-        self.wait_until("2min 45sec")
+        
+        timer.wait_until("2min 45sec")
+        self.play(Indicate(inference_bulletpoints_header["right_keypoint"]), color=RED_E,run_time=2)
 
+        timer.wait_until("2min 51sec")
+        inference_bulletpoint_two = create_inference_bulletpoint_two(self,inference_bulletpoints_header)
 
-        self.wait_until("17min 38sec")
+        timer.wait_until("2min 58sec")
+        create_inference_bulletpoint_two_example(self,inference_bulletpoints_header)
 
+        timer.wait_until("4min 9sec")
+        #timer.wait_until("17min 38sec")
+        
