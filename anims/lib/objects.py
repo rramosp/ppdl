20222bc39ecf8deaafb249b610ec6cebc83ea568
Felipe-RA <felipeangel50@gmail.com>
2022-04-08 20:50:14 -0500

    bayes-theorem-02-example-part-02 animations and stickman object

--- anims/lib/objects.py
+++ anims/lib/objects.py
@@ -445,10 +445,10 @@ def gaussian(color=RED):
     return g
 
 
-def generate_stickman(size = 1.0, general_color = BLACK, color_head = BLACK,
+def generate_stickman(size = 1,fill_opacity=0 ,general_color = BLACK, color_head = BLACK,
                      color_torso = BLACK, color_left_arm = BLACK,
-                     color_right_arm = BLACK, color_left_leg = BLACK, color_right_leg = BLACK,
-                     **kwargs):
+                     color_right_arm = BLACK, color_left_leg = BLACK, color_right_leg = BLACK
+                     ):
 
     """
     A representation of a person using simple shapes. 
@@ -484,23 +484,23 @@ def generate_stickman(size = 1.0, general_color = BLACK, color_head = BLACK,
         color_left_leg = general_color
         color_right_leg = general_color
     
-    stickman_head = Circle(radius = 0.5, color= color_head,**kwargs)
+    stickman_head = Circle(radius = 0.5, color= color_head, fill_opacity = fill_opacity)
 
     ref_point_torso = Dot().move_to(stickman_head.get_bottom()).shift(DOWN)
     ref_point_left_leg = Dot().move_to(ref_point_torso).shift(DOWN+LEFT)
     ref_point_right_leg = Dot().move_to(ref_point_torso).shift(DOWN+RIGHT)
 
-    stickman_torso = Line(start=stickman_head.get_bottom(),end=ref_point_torso.get_center(),buff=0, color=color_torso,**kwargs)
+    stickman_torso = Line(start=stickman_head.get_bottom(),end=ref_point_torso.get_center(),buff=0, color=color_torso)
 
     ref_point_left_arm = Dot().move_to(stickman_torso.get_center()).shift(UP+LEFT)
     ref_point_right_arm = Dot().move_to(stickman_torso.get_center()).shift(UP+RIGHT)
 
 
-    stickman_left_arm = Line(start=stickman_torso.get_center(), end = ref_point_left_arm, buff = 0, color = color_left_arm,**kwargs)
-    stickman_right_arm = Line(start=stickman_torso.get_center(), end = ref_point_right_arm, buff = 0, color = color_right_arm,**kwargs)
+    stickman_left_arm = Line(start=stickman_torso.get_center(), end = ref_point_left_arm, buff = 0, color = color_left_arm)
+    stickman_right_arm = Line(start=stickman_torso.get_center(), end = ref_point_right_arm, buff = 0, color = color_right_arm)
 
-    stickman_left_leg = Line(start=stickman_torso.get_end(), end=ref_point_left_leg, buff= 0, color= color_left_leg,**kwargs)
-    stickman_right_leg = Line(start = stickman_torso.get_end(), end = ref_point_right_leg, buff = 0, color = color_right_leg,**kwargs)
+    stickman_left_leg = Line(start=stickman_torso.get_end(), end=ref_point_left_leg, buff= 0, color= color_left_leg)
+    stickman_right_leg = Line(start = stickman_torso.get_end(), end = ref_point_right_leg, buff = 0, color = color_right_leg)
 
 
 
